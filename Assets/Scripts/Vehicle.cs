6c811b50ade956f2747eba1649312ea8029855b5
Ruben Madsen <ruma2100@student.miun.se>
2022-03-05 22:28:07 +0100

    Inheritance

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    /*
+    string vehicleClass = "Vehicle";
+    string vehicleModel = "";
+    string displayName = "";
+    int armor  = 0;
+    int health = 0;
+    int damage = 0;
+    float damageReduction = 1f;
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.WriteLine("Korv");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
