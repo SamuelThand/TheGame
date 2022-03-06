0c08fc13c667ddac5440bbffdbf878ec212f5265
Ruben Madsen <ruma2100@student.miun.se>
2022-03-06 10:51:35 +0100

    PlayerOrbit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     string handle;
-    Vector2 rotation = Vector2.zero;
-    public float speed = 3;
+    [SerializeField] private Camera cam;
+    private Vector3 orbitPosition = Vector3.zero;
+    private int vOrbitLimit = 360;
+    private int hOrbitLimit = 360;
     //Vehicle me;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.main;
+        cam.transform.position = orbitPosition;
         
-        rotation.y += Input.GetAxis("Mouse X");
-        rotation.x += -Input.GetAxis("Mouse Y");
-        transform.eulerAngles = (Vector2)rotation * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            orbitPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = orbitPosition - cam.ScreenToViewportPoint(Input.mousePosition); //Normalized?
+
+            cam.transform.position = new Vector3();
+            cam.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
+            cam.transform.Rotate(new Vector3(0, 1, 0), direction.x * 180, Space.World);
+            cam.transform.Translate(new Vector3(0, 0, -10));
+
+            orbitPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
     }
 }
