e7db19af249f96e70613aa466a361195e865006c
Samuel Thand <sath2102@student.miun.se>
2022-03-12 13:12:12 +0100

    Structured and cleaned for new build

---
+++ Assets/Scripts/Prefabscripts/Vehicle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Vehicle : MonoBehaviour
+
+{
+    public float topSpeed = 1;
+    public float turnSpeed = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+}
