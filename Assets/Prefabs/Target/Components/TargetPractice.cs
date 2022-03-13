84af3c4f182afc4215076ff09425f8475889e762
Ruben Madsen <ruma2100@student.miun.se>
2022-03-13 17:35:47 +0100

    Finished working customizable boring archery targets

--- Assets/Prefabs/Target/Components/TargetPractice.cs
+++ Assets/Prefabs/Target/Components/TargetPractice.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class TargetPractice : MonoBehaviour
 {
-    enum Diff { hard,medium,easy};
+    enum Diff { ridiculous, easy, normal, hard, impossible };
     // Refs
     [SerializeReference] GameObject target;
+
     // Inputs
-    [SerializeField] int reward = 100;
-    [SerializeField] float difficulty = 1;
-    [SerializeField] bool includeDistanceInReward;
+    [SerializeField] int points = 100;
+    [SerializeField] Diff difficulty = Diff.normal;
+    [SerializeField] bool pointsForDistance;
 
-    //
+    private Renderer renderer;
+
+    // 
+    private float[] scales = { 3.0f, 2.0f, 1.0f, 0.5f, 0.25f };
+    private Color[] colors = { new Color(0.0f, 1.0f, 0.0f), new Color(0.6f, 1.0f, 0.3f), new Color(0.5f, 0.5f, 0.0f), new Color(0.5f, 0.25f, 0.0f), new Color(1.0f, 0.0f, 0.0f) };
     
-    Color color;
-
 
     void Start()
     {
-        
-        //color = target.GetComponent<Material>().color;
-        //color.g = 1f;
-        //target.GetComponent<Material>().color = color;
+        target.transform.localScale = Vector3.one * 1 * scales[(int)difficulty];
+        renderer = target.GetComponent<Renderer>();
+        renderer.material.SetColor("_Color",colors[(int)difficulty]);
+        //renderer.GetComponent<Material>().color = color;
     }
 
     // Update is called once per frame
